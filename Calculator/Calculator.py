f779cb889116135dd1668ed30a31b6bc7f8a9e41
Vladislav Sotnikov <vladislavsotnikov167@gmail.com>
2026-08-06 20:13:07 +0200

    new_python_knowledge

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,30 +1,57 @@
-from decimal import Decimal
 
 
 print("Welcome to the Calculator")
-print("Enter first number")
-a=Decimal(input()) #e.g.:pi 3.14
-print("Enter second number")
-b=Decimal(input()) #e.g.: e 2.72
+goon="y" #go on = yes
+while (goon=="y"):
+    print("Enter first number")
+    a=float(input()) #e.g.:pi 3.14
+    print("Enter second number")
+    b=float(input()) #e.g.: e 2.72
+
+    print("Choose operation: +, -, *, /")
+    print("1 for addition")
+    print("2 for substraction")
+    print("3 for multiplication")
+    print("4 for division")
+    oper=int(input())
 
 
-sum=round(a+b,2)
-difference=round(a-b,2)
-product=round(a*b,2)
+    #if, elif, else variant
+    """if oper==1: resultoper=a+b
+    elif oper==2: resultoper=a-b
+    elif oper==3: resultoper=a*b
+    elif oper==4:
+        if b!=0: resultoper=a/b
+        else:
+            print("Can't divide by 0")
+            resultoper="N/A"
+    else: print("Impossible operation")
+    
+    if oper>0 and oper<5: print(f"Result: {resultoper}")
+    else: print("Try again.")"""
 
 
-print(f"Sum: {sum}")
-print(f"Difference: {difference}")
-print(f"Product: {product}")
+    #match-case variant
+    match oper:
+        case 1:
+            resultoper=a+b
+            print(resultoper)
+        case 2:
+            resultoper=a-b
+            print(resultoper)
+        case 3:
+            resultoper=a*b
+            print(resultoper)
+        case 4:
+            if b==0:
+                print("Can't divide by 0")
+            else:
+                resultoper = a / b
+                print(resultoper)
+        case _:
+            print("Impossible operation")
 
-if b!=0:
-    quotient=round(a/b,2)
-    print(f"Quotient:{quotient}")
-else:
-    print("Equals: Can't divide by zero")
-
-
-
+    goon=input("Do you wish to try again? [y/n]: ").strip().lower()
 print("Thank you for using Calculator")
 
 
